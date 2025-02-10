eec183426cc87bdf19ee971c33c1284a43d8e754
ceErtugrulAydin <ce.ertugrulaydin@gmail.com>
2025-02-11 00:38:28 +0300

    all exception filter added

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,12 +8,15 @@ import {
   Param,
   Post,
   Put,
+  UseFilters,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserCreateDto } from 'tools/dtos/user.dto';
 import { UserModel } from 'tools/models/user.model';
+import { AllExceptionFilter } from 'libs/filters/all-exception.filter';
 
 @Controller('user')
+@UseFilters(AllExceptionFilter)
 export class UserController {
   constructor(private userService: UserService) {}
 
